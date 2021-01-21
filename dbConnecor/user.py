8332e3464758a4374dc5d3b4c1b87d2d7537857c
Marcel <marceli.lagoda@gmail.com>
2021-01-21 18:29:11 +0100

    Import Cursor from database, modify Class User

--- dbConnecor/user.py
+++ dbConnecor/user.py
@@ -1,4 +1,4 @@
-from database import Connection
+from database import Cursor
 
 
 class User:
@@ -12,22 +12,20 @@ class User:
         return f"{self.email}"
 
     def insert_into_db(self):
-        with Connection() as cnx:
-            with cnx.cursor() as cursor:
-                cursor.execute(
-                    "INSERT INTO users (email, first_name, last_name) VALUES(%s, %s, %s)",
-                    (self.email, self.f_name, self.l_name),
-                )
+        with Cursor() as cursor:
+            cursor.execute(
+                "INSERT INTO users (email, first_name, last_name) VALUES(%s, %s, %s)",
+                (self.email, self.f_name, self.l_name),
+            )
 
     @classmethod
     def select_from_db_by_email(cls, email):
-        with Connection() as cnx:
-            with cnx.cursor() as cursor:
-                cursor.execute("SELECT * FROM users WHERE email=%s", (email,))
-                selected_data = cursor.fetchone()
-                return cls(
-                    selected_data[0],
-                    selected_data[1],
-                    selected_data[2],
-                    selected_data[3],
-                )
+        with Cursor() as cursor:
+            cursor.execute("SELECT * FROM users WHERE email=%s", (email,))
+            selected_data = cursor.fetchone()
+            return cls(
+                selected_data[0],
+                selected_data[1],
+                selected_data[2],
+                selected_data[3],
+            )
