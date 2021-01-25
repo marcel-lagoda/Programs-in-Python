66d57c193bd9691c986c06a80aa8c8a7921662e7
Marcel <marceli.lagoda@gmail.com>
2021-01-25 13:42:15 +0100

    Add add_movie method

--- movie_rental_app/user.py
+++ movie_rental_app/user.py
@@ -1,15 +1,17 @@
 from movie import Movie
 
+
 class User:
     def __init__(self, name):
         self.name = name
         self.movies = []
 
     def __repr__(self):
-        return f'{self.name}'
+        return f"{self.name}"
 
-    def add_movie(self):
-        pass
+    def add_movie(self, genre=None, title=None):
+        movie = Movie(title, genre, False)
+        self.movies.append(movie)
 
     def delete_movie(self):
         pass
